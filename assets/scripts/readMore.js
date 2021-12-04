a0761d10b3e03605f1c08dc4b0dbbf4746545f0c
bravo <adeolaisrael424@gmail.com>
2021-12-04 21:55:07 +0100

    worked more on readmore togglers

--- assets/scripts/readMore.js
+++ assets/scripts/readMore.js
@@ -3,13 +3,6 @@ function readMore() {
     toggler = event.target
     // console.log(toggler)
 
-    // Changing the Button Text
-    if (toggler.textContent == "Read More") {
-        toggler.textContent = "Read Less"
-    } else {
-        toggler.textContent = "Read More"
-    }
-
     // Element Container 
     togglerContainer = toggler.parentNode;
     // console.log(togglerContainer);
@@ -17,7 +10,13 @@ function readMore() {
     // The Secondary Content inside the Container
     moreContent = togglerContainer.querySelector('.hidden-by-default');
     // console.log(moreContent)
-
     moreContent.classList.toggle('shown');
+
+    // Changing the Button Text
+    if (toggler.textContent == "Read More") {
+        toggler.textContent = "Read Less"
+    } else {
+        toggler.textContent = "Read More"
+    }
 
 }
