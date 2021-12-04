bfd44bec310e454471d86ef850b6889c293a7159
bravo <adeolaisrael424@gmail.com>
2021-12-04 12:49:04 +0100

    Worked on read more button about section

--- assets/scripts/readMore.js
+++ assets/scripts/readMore.js
@@ -1,4 +1,23 @@
 function readMore() {
-    container = parent
-    console.log(container)
+    // Gets the Element Clicked
+    toggler = event.target
+    // console.log(toggler)
+
+    // Changing the Button Text
+    if (toggler.textContent == "Read More") {
+        toggler.textContent = "Read Less"
+    } else {
+        toggler.textContent = "Read More"
+    }
+
+    // Element Container 
+    togglerContainer = toggler.parentNode;
+    // console.log(togglerContainer);
+
+    // The Secondary Content inside the Container
+    moreContent = togglerContainer.querySelector('.hidden-by-default');
+    // console.log(moreContent)
+
+    moreContent.classList.toggle('shown');
+
 }
